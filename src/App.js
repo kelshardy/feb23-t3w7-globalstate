177e4232340ded238f3063bd3ae2d9e726461eea
kelshardy <14024@coderacademy.edu.au>
2023-09-30 10:56:52 +1000

    fix: tidy up connection context into its own component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ApiTester from './ApiTester';
 import ConnectionContext, { defaultConnectionData } from './context/ConnectionContext';
 import UserGlobalData from './context/UserContext';
 import UserDisplay from './UserDisplay';
+import ConnectionProvider from './context/ConnectionContext';
 
 function App() {
 
@@ -12,12 +13,10 @@ function App() {
     <div className="App">
 
       <UserGlobalData>
-        <ConnectionContext.Provider value={defaultConnectionData}>
-          
+        <ConnectionProvider>
           <ApiTester />
           <UserDisplay />
-          
-        </ConnectionContext.Provider>
+        </ConnectionProvider>
       </UserGlobalData>
 
     </div>
